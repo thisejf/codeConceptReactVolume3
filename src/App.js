6b2cf362cd9c33bd97f3a3ee3ee13776f1a668ef
ThiseJF <thisejf@gmail.com>
2021-03-29 17:10:59 +0200

    fetch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Todos from "./components/Todos";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+    state = {
+        todos: []
+    }
+
+    componentDidMount() {
+        this.getTodos()
+            .then(res => res.json())
+            .then(data => {
+                this.setState({
+                    todos: data
+                });
+            });
+    }
+
+    getTodos () {
+        return fetch('https://jsonplaceholder.typicode.com/todos');
+    }
+
+    render(){
+        console.log(this.state.todos);
+        return (
+            <div className="App">
+                <header className="App-header">
+                    Le routage
+                </header>
+                <Todos />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/Todos.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Todos = (props) => {
+    return (
+        <div>
+            <ul>
+                <li>d</li>
+                <li>f</li>
+                <li>g</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Todos;
